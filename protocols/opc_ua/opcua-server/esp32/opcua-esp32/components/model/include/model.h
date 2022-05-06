3a92712da8a184b260835c0405217ffbd228cf8d
WILO-CSW <93249277+WILO-CSW@users.noreply.github.com>
2022-05-06 09:07:07 +0200

    update model.c und .h
    added hall sensor

--- protocols/opc_ua/opcua-server/esp32/opcua-esp32/components/model/include/model.h
+++ protocols/opc_ua/opcua-server/esp32/opcua-esp32/components/model/include/model.h
@@ -20,6 +20,18 @@
 // void
 // addLEDMethod(UA_Server *server);
 
+
+/* Hall sensor */
+UA_StatusCode
+readCurrentHall(UA_Server *server,
+                const UA_NodeId *sessionId, void *sessionContext,
+                const UA_NodeId *nodeId, void *nodeContext,
+                UA_Boolean sourceTimeStamp, const UA_NumericRange *range,
+                UA_DataValue *dataValue);
+
+void
+addCurrentHallDataSourceVariable(UA_Server *server);
+
 /* Temperature */
 UA_StatusCode
 readCurrentTemperature(UA_Server *server,
