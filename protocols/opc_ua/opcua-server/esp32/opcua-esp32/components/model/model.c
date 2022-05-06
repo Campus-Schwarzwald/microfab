f6942beb241ba873d78bce90894fd4e5b24f11e5
WILO-CSW <93249277+WILO-CSW@users.noreply.github.com>
2022-05-06 09:05:38 +0200

    Update model.c

--- protocols/opc_ua/opcua-server/esp32/opcua-esp32/components/model/model.c
+++ protocols/opc_ua/opcua-server/esp32/opcua-esp32/components/model/model.c
@@ -89,6 +89,42 @@ configureGPIO(void) {
 //                             1, &inputArgument, 1, &outputArgument, NULL, &createdNodeId);
 // }
 
+/* Hall sensor */
+
+UA_StatusCode
+readCurrentHall(UA_Server *server,
+                const UA_NodeId *sessionId, void *sessionContext,
+                const UA_NodeId *nodeId, void *nodeContext,
+                UA_Boolean sourceTimeStamp, const UA_NumericRange *range,
+                UA_DataValue *dataValue) {
+    UA_Float hall = hallRead();
+    UA_Variant_setScalarCopy(&dataValue->value, &hall,
+                             &UA_TYPES[UA_TYPES_FLOAT]);
+    dataValue->hasValue = true;
+    return UA_STATUSCODE_GOOD;
+}
+
+
+void
+addCurrentHallDataSourceVariable(UA_Server *server) {
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    attr.displayName = UA_LOCALIZEDTEXT("en-US", "hall");
+    attr.dataType = UA_TYPES[UA_TYPES_FLOAT].typeId;
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ;
+
+    UA_NodeId currentNodeId = UA_NODEID_STRING(1, "hall");
+    UA_QualifiedName currentName = UA_QUALIFIEDNAME(1, "hall sensor value");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_NodeId variableTypeNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE);
+
+    UA_DataSource timeDataSource;
+    timeDataSource.read = readCurrentHall;
+    UA_Server_addDataSourceVariableNode(server, currentNodeId, parentNodeId,
+                                        parentReferenceNodeId, currentName,
+                                        variableTypeNodeId, attr,
+                                        timeDataSource, NULL, NULL);
+}
 
 /* Temperature */
 
